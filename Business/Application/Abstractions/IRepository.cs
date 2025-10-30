eb5020117b0f3046b2d8ed291233e7f2328d999d
Omemo7 <omarabuhadhoud7@gmail.com>
2025-10-30 17:41:40 +0300

    Add IRepository interface and update project structure
    Removed the `Application\Abstractions\` folder from `Business.csproj` as it is no longer needed or has been refactored. Introduced a new `IRepository<TEntity>` interface in `IRepository.cs` within the `Business.Application.Abstractions` namespace, implementing a generic repository pattern with asynchronous CRUD operations.

---
+++ Business/Application/Abstractions/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Business.Application.Abstractions;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetById(Guid id);
+    Task Add(TEntity entity);
+    Task Update(TEntity entity);
+    Task Remove(Guid id);
+}
